b63d2f828347769b5322bbabda8c8739c225ebc1
Anton Sharrp Furin <sharrrrp@gmail.com>
2014-03-22 21:27:59 +0400

    Add name for devices

--- Alfred Workflow/url_notifier.py
+++ Alfred Workflow/url_notifier.py
@@ -44,7 +44,6 @@ def make_item(uid, arg, title, subtitle=''):
         'arg': arg,
         'title': title,
         'subtitle': subtitle
-        # 'icon': path_to_icon
     }
     return dic
 
@@ -58,7 +57,7 @@ def help_item(title='Help'):
 # Get all registered devices
 def get_devices():
     if not os.path.isfile(devices_path):
-        return []
+        return {}
     f = open(devices_path, 'r')
     devices = json.loads(f.read())
     f.close()
@@ -84,38 +83,47 @@ def process(query):
                 items.append(help_item('You didn\'t add devices'))
             else:
                 # send to all
-                json_devices = json.dumps(devices)
+                json_devices = json.dumps([d for d in devices])
                 arg = '{"command":"send","url":"' + query + '","udids":' + json_devices + '}'
                 items.append(make_item('1', arg, 'Push to all', 'Send url to all registered devices'))
                 # send to specific
                 num = 2
                 for d in devices:
                     arg = '{"command":"send","url":"' + query + '","udids":["' + d + '"]}'
-                    items.append(make_item(str(num), arg, 'Send to '+ d))
+                    items.append(make_item(str(num), arg, 'Send to '+ devices[d], 'Device id: ' + d))
                     num += 1
-    elif len(parts) == 2: # add / remove commands
-        if parts[0] == 'add':
-            if len(parts[1]) > 4: # the shortest udid is 5 chars long
-                devices = get_devices()
-                if parts[1] in devices: # device already added
-                    items.append(help_item('Device already added'))
-                else:
-                    arg = '{"command":"add","udid":"' + parts[1] + '"}'
-                    items.append(make_item('1', arg, 'Add this device'))
-            else:
-                items.append(help_item('Not a valid device id'))
-        elif parts[0] == 'rm':
+    elif len(parts) == 2: # remove command and hint for add command
+        if parts[0] == 'rm':
             devices = get_devices()
             num = 1
+            if len(devices) == 0:
+                items.append(help_item('No devices added yet'))
             for d in devices: # filter existing devices
-                if parts[1] in d:
+                if parts[1] in d or parts[1] in devices[d]:
                     arg = '{"command":"remove","udid":"' + d + '"}'
-                    items.append(make_item(str(num), arg, 'Remove '+ d + ' device'))
+                    items.append(make_item(str(num), arg, 'Remove '+ devices[d], 'Device id: ' + d))
                     num += 1
             if len(items) == 0:
-                items.append(help_item('No devices with "' + parts[1] + '" in id'))
+                items.append(help_item('No devices with "' + parts[1] + '" in id or name'))
+        elif parts[0] == 'add':
+            items.append(help_item('Enter device id and name'))
         else:
             items.append(help_item('Unknown command'))
+    elif len(parts) == 3:
+        if parts[0] == 'add':
+            if len(parts[1]) > 4: # the shortest udid is 5 chars long
+                if len(parts[2]) < 1:
+                    items.append(help_item('Enter device name'))
+                else:
+                    devices = get_devices()
+                    if parts[1] in devices or parts[2] in devices.values(): # device already added
+                        items.append(help_item('Device already added'))
+                    else:
+                        arg = '{"command":"add","udid":"' + parts[1] + \
+                            '","name":"' + parts[2] + '"}'
+                        items.append(make_item('1', arg, 'Add this device'))
+            else:
+                items.append(help_item('Not a valid device id'))
     else:
         items.append(help_item('Too much parameters'))
 
@@ -134,8 +142,8 @@ def execute(command):
     elif data['command'] == 'add':
         # Checking that udid exists
         url = 'http://188.226.174.130:5000/client/'
-        del(data['command'])
-        req = urllib2.Request(url=url, data=json.dumps(data))
+        params = {'udid':data['udid']}
+        req = urllib2.Request(url=url, data=json.dumps(params))
         req.add_header('Content-Type', 'application/json')
         r = urllib2.urlopen(req)
         response = json.loads(r.read())
@@ -143,14 +151,15 @@ def execute(command):
             print('No registered device with id "' + data['udid'] + '"')
         else:
             devices = get_devices()
-            devices.append(data['udid'])
+            devices[data['udid']] = data['name']
             write_devices(devices)
-            print('Device ' + data['udid'] + ' added')
+            print('Device "' + data['name'] + '" with id "' + data['udid'] + '" added')
     elif data['command'] == 'remove':
         devices = get_devices()
-        devices.remove(data['udid'])
+        name = devices[data['udid']]
+        del(devices[data['udid']])
         write_devices(devices)
-        print('Device ' + data['udid'] + ' removed')
+        print('Device ' + name + 'with id "' + data['udid'] + '" removed')
     elif data['command'] == 'openurl':
         import webbrowser
         new = 2 # open in a new tab, if possible
